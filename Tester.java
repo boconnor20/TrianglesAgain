61ac073a903816739566b1a0db1a1de002ea6ed3
Brandon O'Connor <boconnor20@stuy.edu>
2020-11-01 21:56:53 -0500

    created and tested initial point methods

---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+  public static void main(String[] args) {
+    Point p1= new Point(3.0,4.0);
+    Point p2= p1;
+    System.out.println(p2.getX());
+    System.out.println(p2.getY());
+  }
+}
